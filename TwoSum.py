9c3e1c969f48b87c0da8feac6c5fee3cc060979b
samke-glitch <sammythemwa@gmail.com>
2024-05-14 21:20:44 +0300

    towsum

---
+++ TwoSum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Given an array of integers, return indices of the two numbers
+such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may
+not use the same element twice.
+Example:
+    Given numbs = [2, 7, 11], target = 9,
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+"""
