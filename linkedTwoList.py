61c5104e7092661d90474ad5c826699070c44829
samke-glitch <sammythemwa@gmail.com>
2024-05-20 21:04:18 +0300

    edit comment

--- linkedTwoList.py
+++ linkedTwoList.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-you are given two non-empty linked lists representing two non-negative
+You are given two non-empty linked lists representing two non-negative
 integers. The digits are stored in reverse order and each of their nodes contain a single
 digit. Add the two numbers and return it as a linked list.
 you may assume the two numbers do not contain any leading zero,
