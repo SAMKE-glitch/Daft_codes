ddac9c4dfc4d29826671e4eaa23ee2d79805cbdf
samke-glitch <sammythemwa@gmail.com>
2024-05-20 21:16:03 +0300

    afresh

--- linkedTwoList.py
+++ linkedTwoList.py
@@ -18,36 +18,7 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        added = ListNode(val = (l1.val + l2.val) % 10)
-        carry_over = (l1.val + l2.val) // 10
-        current_node = added
 
-        while(l1.next and l2.next):
-            l1 = l1.next
-            l2 = l1.next
-
-            current_node.next = ListNode(val = (carry_over + l1.val + l2.val) % 10)
-            carry_over = (carry_over + l1.val + l2.val) // 10
-            current_node = current_node.next
-
-        while(l1.next):
-            l1 = l1.next
-
-            current_node.next = ListNode(val = (carry_over + l1.val) % 10)
-            carry_over = (carry_over + l1.val) // 10
-            current_node = current_node.next
-
-        while(l1.next):
-            l1 = l1.next
-
-            current_node.next = ListNode(val = (carry_over + l1.val) % 10)
-            carry_over = (carry_over + l1.val) // 10
-            current_node = current_node.next
-
-        if (carry_over) > 0:
-            current_node.next = ListNode(1)
-
-        return(added)
 
 
 # Create the first linked list representing the number 342
