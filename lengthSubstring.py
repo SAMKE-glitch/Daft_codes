f7d72737557b4ccd51688ff54373e0863f02e26b
samke-glitch <sammythemwa@gmail.com>
2024-05-23 20:59:17 +0300

    substrinf example

--- lengthSubstring.py
+++ lengthSubstring.py
@@ -1,4 +1,10 @@
 #!/usr/bin.env python3
 """
+Given a string, find the length of the longest substring
+without repeating characters
+Example 1:
+Inpt: "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3
 """
 
