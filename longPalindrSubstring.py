869e44080c829dce6e4b1a8195fa8b9f7608dfca
samke-glitch <sammythemwa@gmail.com>
2024-05-29 21:13:41 +0300

    palindromic substring

---
+++ longPalindrSubstring.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Given a string s, return the longest palindromic substring in s
+Example 1:
+    Input: s = "babad"
+    Output: "bab"
+    Note: "aba" is also a valid answer
+
+Example 2:
+    Input: s = "cbbd"
+    Output: "bb"
+"""
