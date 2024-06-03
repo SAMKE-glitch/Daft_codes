1134bfd8fe3d7970031f9885a71a941f4c7a6a98
samke-glitch <sammythemwa@gmail.com>
2024-06-03 21:12:03 +0300

    redoing task

--- longPalindrSubstring.py
+++ longPalindrSubstring.py
@@ -24,11 +24,7 @@ Constraints:
 class Solution():
     def longestPalindrome(self, s: str) -> str:
 
-        def check_palin(s):
-            return (s == s[::-1])
+        def check_palin(s: str) -> bool:
+            return(s == s[::-1])
 
-    # check all substrings
-    for length in range(len(S), 0, -1):
-        for start_index in range(0, len(s) + 1 - length):
-            if check_palin(s[start_index: (start_index + length)]):
-                return s[start_index: (start_index + length)]
+        
