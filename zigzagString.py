b4796b66b58ce0c33613212b5d19d8c38217594f
samke-glitch <sammythemwa@gmail.com>
2024-06-09 21:40:04 +0300

    zigzag

--- zigzagString.py
+++ zigzagString.py
@@ -29,4 +29,24 @@ Example 3:
 """
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        row_map = {row:"" for row in range(1, numRows+1)}
+
+        row = 1
+        up = True
+
+        for letter in s:
+            row_map[row] += letter
+            if (row == 1) or ((row < numRows) and up):
+                row += 1
+                up = True
+            else:
+                row -= 1
+                up = False
+        coverted = ""
+
+        for row in range(1, numRows+1):
+            converted += row_map[row]
+        return converted
 
