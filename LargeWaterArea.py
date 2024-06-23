4156ce1b406db1f65273090c4c363bf54a05e184
samke-glitch <sammythemwa@gmail.com>
2024-06-23 21:59:09 +0300

    optimize

--- LargeWaterArea.py
+++ LargeWaterArea.py
@@ -19,8 +19,16 @@ class Solution():
         start = 0
         end = len(height) - 1
         largest = 0
+        prev_start = 0
+        prev_end = 0
 
         while start != end:
+            if height[start] < prev_start:
+                start += 1
+                continue
+            if height[end] < prev_end:
+                end -= 1
+                continue
             new_area = min(height[start], height[end]) * (end - start)
 
             if new_area > largest:
