28d4aab2041f1e432bfbc4770c3c8b669ea8bf6b
samke-glitch <sammythemwa@gmail.com>
2024-06-30 20:40:42 +0300

    edit

--- LargeWaterArea.py
+++ LargeWaterArea.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-
-ven n non-negative integers a1, a2, ... An, where each represents
+given n non-negative integers a1, a2, ... An, where each represents
 a point at cordinate (i, ai). n vertical lines are drawn such that two endpoints
 of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container
 such that the container contains the most water.
