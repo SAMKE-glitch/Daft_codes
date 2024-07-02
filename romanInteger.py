b73e5f359c31b655ea8cfac21a7bb8596b7f2e72
samke-glitch <sammythemwa@gmail.com>
2024-07-02 21:57:38 +0300

    table dict

--- romanInteger.py
+++ romanInteger.py
@@ -48,4 +48,10 @@ Constraints:
 """
 class Solution:
     def romanToInt(self, s: str) -> int:
-
+        roman_table = {"I": 1,
+                       "V": 5,
+                       "X": 10,
+                       "L": 50,
+                       "C": 100,
+                       "D": 500,
+                       "M": 1000}
