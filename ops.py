797e551c6b7126d6cb5bd9a34d9ced0c702c0796
samke-glitch <sammythemwa@gmail.com>
2024-07-06 11:58:34 +0300

    result

--- ops.py
+++ ops.py
@@ -4,6 +4,7 @@ calPoints
 """
 def calPoints(ops) -> int:
     stack = []
+    result = None
     for op in ops:
         if op == "+":
             stack.append(stack[-1] + stack[-2])
@@ -13,7 +14,8 @@ def calPoints(ops) -> int:
             stack.append(2 * stack[-1])
         else:
             stack.append(int(op))
-    return sum(stack)
+    result = sum(stack)
+    return result
 
 if __name__ == "__main__":
     ops1 = ["5", "-2", "4", "C", "D", "9", "+", "+"]
