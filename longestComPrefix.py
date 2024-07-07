ee95e51f1533b052e525f4f2a1407f8eb607dfae
samke-glitch <sammythemwa@gmail.com>
2024-07-07 21:16:17 +0300

    longest common prefix

---
+++ longestComPrefix.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Write a function to find the longest common prefix string amongst an array of
+strings.
+If there is no common prefix, return an empty string "".
+Example 1:
+    Input: ["flower", "flow", "flight"]
+    Output: "fl"
