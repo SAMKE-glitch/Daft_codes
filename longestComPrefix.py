0a3a20e6c2c94897310d55c492e7356c7424a1ee
samke-glitch <sammythemwa@gmail.com>
2024-07-07 21:20:05 +0300

    commits

--- longestComPrefix.py
+++ longestComPrefix.py
@@ -6,3 +6,13 @@ If there is no common prefix, return an empty string "".
 Example 1:
     Input: ["flower", "flow", "flight"]
     Output: "fl"
+
+Example 2:
+    Input: ["dog", "racecar", "car"]
+    Output: ""
+    Explanation: There is no common prefix among the input strings.
+
+NTE:
+    All given inputs are in lowercase letters a-z.
+"""
+
