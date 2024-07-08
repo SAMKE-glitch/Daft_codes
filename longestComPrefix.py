23440db80711e306c0f8f0f79b7d781d4cd63f79
samke-glitch <sammythemwa@gmail.com>
2024-07-08 21:59:19 +0300

    solution

--- longestComPrefix.py
+++ longestComPrefix.py
@@ -22,3 +22,14 @@ class Solution():
         if len(strs) == 1:
             return(strs[0])
 
+        pref = strs[0]
+        plen = len(pref)
+
+        for s in strs[1:]:
+            while pref != s[0: plen]:
+                pref = pref[0: (plen - 1)]
+                plen -= 1
+
+                if plen == 0:
+                    return("")
+        return pref
