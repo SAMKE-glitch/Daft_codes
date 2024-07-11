e92079fb69bd63fa8c46cc0688769d5225c8dff9
samke-glitch <sammythemwa@gmail.com>
2024-07-11 21:50:00 +0300

    n^3 solution

--- ThreeSum.py
+++ ThreeSum.py
@@ -28,4 +28,11 @@ class Solution:
         if len(nums) < 3:
             return([])
         #n^3 Triple for loop solution
+        for i in range(0, len(nums) - 2):
+            for j in range(i+1, len(nums) - 1):
+                for k in range(j+1, len(nums)):
+                    if (nums[i] + nums[j] + nums[k]) == 0:
+                        triplets.append(tuple(sorted([nums[i] , nums[j] , nums[k]])))
+        return(list(set(triplets)))
+
 
