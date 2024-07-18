b4a0b8da14755facd0e6cb510e35ced23eca656f
samke-glitch <sammythemwa@gmail.com>
2024-07-18 21:41:04 +0300

    three sum closest

---
+++ 3SumClosest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Given an array numsof n integers and an integer target, find three integers
+in nums such that the nums is closet to target. Return the sum of the three integers.
+You may assume that each input would have exactly one solution
+
+Example 1:
+
+"""
