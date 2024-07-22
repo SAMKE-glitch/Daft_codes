5d74f39e32e617824bedee6fc6b3563e54ee361e
samke-glitch <sammythemwa@gmail.com>
2024-07-22 21:31:02 +0300

    examples

--- letterCombinaPhonenu.py
+++ letterCombinaPhonenu.py
@@ -4,4 +4,14 @@ Given a string containing digit from 2-9 inclusive, return all possible letter
 combinations that the number could represent. Return the answer in any order.
 A mappinf of digit to letters(just like on the telephone buttons) is given below.
 Note that 1 does not map to any letters
+Example 1:
+    Input: digit = "23"
+    output: ["ad", "ae", "af", "bd", "be", "cd", "ce", "cf"]
+
+Example 2:
+    Input: digits = ""
+    Output: []
+
+Example 3:
+
 """
