a1023c37603d0e91d95b6b9ba23377810bfa9ad2
samke-glitch <sammythemwa@gmail.com>
2024-07-22 21:27:03 +0300

    phone numbers

---
+++ letterCombinaPhonenu.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Given a string containing digit from 2-9 inclusive, return all possible letter
+combinations that the number could represent. Return the answer in any order.
+A mappinf of digit to letters(just like on the telephone buttons) is given below.
+Note that 1 does not map to any letters
+"""
