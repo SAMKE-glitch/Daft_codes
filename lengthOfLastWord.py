a6951c6644431d63fcb63898544e3b8cf96d4b92
samke-glitch <sammythemwa@gmail.com>
2024-08-02 21:36:57 +0300

    length of last string

---
+++ lengthOfLastWord.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Given a string S consits of some words separated by spaces, return the length of the last word in the string.
+If the last word does not exist, return 0.
+Aword is a maximal substring consisting of non-space characters only.
+
+Examples 1:
+    Input: s = "Hello World"
+    Output: 5
+
+Example 2:
+    Input: s = ""
+    Output: 0
+"""
