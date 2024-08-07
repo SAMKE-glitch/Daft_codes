848c024d6bd15f0729cec2a445c8a302cc6e6243
samke-glitch <sammythemwa@gmail.com>
2024-08-07 21:39:05 +0300

    example 1

--- plusOne.py
+++ plusOne.py
@@ -5,4 +5,8 @@ increment one to the integer.
 The digits are stored such that the most significant digit is at the head of the
 list, and each element in the array contains a single digit.
 You may assume the integer does not contain any leading zero, except the number 0 itself.
+Example 1:
+    Input: digits = [1, 2, 3]
+    Output: [1, 2, 3]
+    Explanation: The array represents the integer 123.
 """
