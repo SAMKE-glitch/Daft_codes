0f49ce43912fea6483a31536997f5c33f73aeb2b
samke-glitch <sammythemwa@gmail.com>
2024-08-09 21:36:25 +0300

    version 2

--- plusOne.py
+++ plusOne.py
@@ -28,6 +28,7 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # method 2: reverse list edition
         # Method 1: Type conversion
         return [int(x) for x in str(int("".join([str(i) for i in digits])) + 1)]
 
