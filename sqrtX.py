1764793fd8ddeec4aceb354c488310cbb409361e
samke-glitch <sammythemwa@gmail.com>
2024-08-12 21:43:12 +0300

    example

--- sqrtX.py
+++ sqrtX.py
@@ -9,5 +9,9 @@ Example 1:
     Output: 2
 
 Example 2:
+    Input: x = 8
+    Output: 2
+    Explanation: The square root of 8 is 2.82842..., and since the decimal part
+    is truncated, 2 is returned.
 
 """
