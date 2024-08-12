4343394b4d1daf4422943eada463b94bb9e558f7
samke-glitch <sammythemwa@gmail.com>
2024-08-12 21:40:30 +0300

    sqrt

---
+++ sqrtX.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Given a non-negative integer x, compute and return the sqaure root of x.
+Since the return is an integer, the decimal digits are truncated, and only
+the integer part of the result is returned.
+
+Example 1:
+    Input: x = 4
+    Output: 2
+
+Example 2:
+
+"""
