83d370a08e6c054260f710746adc23f8b758f5d4
samke-glitch <sammythemwa@gmail.com>
2024-08-13 21:44:20 +0300

    example 2

--- climbinStairs.py
+++ climbinStairs.py
@@ -10,4 +10,12 @@ Example 1:
     Explanation: There are two ways to climb to the top
     1. 1 step + 1 step
     2. 2 steps
+
+Example 2:
+    Input: 3
+    Output: 3
+    Explanation: There are three ways to climb to the top.
+    1. 1 step + 1 step + 1 step
+    2. 1 step + 2 steps
+    3. 2 steps + 1 step
 """
