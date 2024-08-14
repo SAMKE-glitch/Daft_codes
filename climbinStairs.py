7ff47df2b959c366c701ce28b478ae05eba2b418
samke-glitch <sammythemwa@gmail.com>
2024-08-14 21:57:54 +0300

    debug

--- climbinStairs.py
+++ climbinStairs.py
@@ -24,8 +24,13 @@ Constraints:
 """
 class Solution:
     def climbStairs(self, n: int) -> int:
-        path {1:1, 2:2, 3:3}
+        path = {1: 1, 2: 2, 3: 3}
 
         for x in range(4, n + 1):
             path[x] = path[x-1] + path[x-2]
         return(path[n])
+
+sam = Solution()
+Input = 44
+result = sam.climbStairs(Input)
+print(result)
