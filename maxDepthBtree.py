9159ca7f9900edf347849b4d871049c6e1e372c5
samke-glitch <sammythemwa@gmail.com>
2024-08-16 21:44:36 +0300

    class

--- maxDepthBtree.py
+++ maxDepthBtree.py
@@ -7,4 +7,15 @@ NOTE: A leaf is a node with no children
 
 Example: Given binary tree [3, 9, 20, null, null, 15, 7],
 return its depth = 3
+
 """
+# Defination for a binary tree node
+# class TreeNode:
+#       def __init__(self, val=0, left=None, right=None):
+#            self.val = val
+#            self.left = left
+#            self.right = right
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+
