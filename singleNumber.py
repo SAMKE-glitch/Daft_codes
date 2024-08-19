7dee7b24dcc776158bec263ea5403a09a1deda09
samke-glitch <sammythemwa@gmail.com>
2024-08-19 21:25:13 +0300

    single number

---
+++ singleNumber.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Given a non-empty array of integers, every element appears twice except for one.
+Find that single one.
+
+NOTE: You algorithm should have a linear runtime complexity. Could you implement
+it without using extra memory?
+"""
