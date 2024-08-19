14612c2d89793eb73052fe70de1b259e3b1731cb
samke-glitch <sammythemwa@gmail.com>
2024-08-19 21:18:26 +0300

    comment

--- maxDepthBtree.py
+++ maxDepthBtree.py
@@ -10,11 +10,11 @@ return its depth = 3
 
 """
 # Defination for a binary tree node
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+#class TreeNode:
+#    def __init__(self, val=0, left=None, right=None):
+#        self.val = val
+#        self.left = left
+#        self.right = right
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
