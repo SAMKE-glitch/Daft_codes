a7bef8b69ba3551230eb3b558b5d84cab9509941
samke-glitch <sammythemwa@gmail.com>
2024-08-19 21:35:29 +0300

    example 2

--- singleNumber.py
+++ singleNumber.py
@@ -6,7 +6,11 @@ Find that single one.
 NOTE: You algorithm should have a linear runtime complexity. Could you implement
 it without using extra memory?
 
-Example:
+Example 1:
     Input: [2,2,1]
     Output: 1
+
+Example 2:
+    Input: [4,1,2,1,2]
+    Output: 4
 """
