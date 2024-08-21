f547f6b12ef4c393674cfb699db7190b30412329
samke-glitch <sammythemwa@gmail.com>
2024-08-21 21:18:13 +0300

    example 1

--- majorityElement.py
+++ majorityElement.py
@@ -2,6 +2,12 @@
 """
 Given an array of size n, find the majority element. The majority element is the element
 that appears more than n/2 | times.
-You may assume that the array is non-empty and the majority element always exist in the array.
+You may assume that the array is non-empty and the majority element alwaysexist in the array.
+
+Example 1:
+    Input: [3,2,3]
+    Output: 3
+
+Example 2:
 
 """
