3fc9737791d0ca919ea443497e205f9680b8fc54
samke-glitch <sammythemwa@gmail.com>
2024-08-26 21:29:47 +0300

    method

--- MoveZeroes.py
+++ MoveZeroes.py
@@ -11,4 +11,11 @@ Note:
     1. You must do this in-place without making a copy of the array.
     2. Minimize the total number of operations.
 """
+from typing import List
+
+
 class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead
+        """
