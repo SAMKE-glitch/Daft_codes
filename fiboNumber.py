3c11f324913bac06c19b08d5edc1dc5cd35272fb
samke-glitch <sammythemwa@gmail.com>
2024-08-28 21:15:29 +0300

    fibo

---
+++ fiboNumber.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+The Fibonacci numbers commonly denoted F(n) form a sequence, called the Fibonacci
+sequence, such that each number is the sum of the two preceding ones, starting
+from 0 and 1. That is
+"""
