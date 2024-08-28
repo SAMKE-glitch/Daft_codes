8e637fed8a70b4fd1c2b254d929b62a56f22a6e9
samke-glitch <sammythemwa@gmail.com>
2024-08-28 21:33:08 +0300

    example 1

--- fiboNumber.py
+++ fiboNumber.py
@@ -7,4 +7,9 @@ F(0) = 0,   F(1) = 1
 F(N) = F(N - 1) + F(N - 2),     for N > 1.
 
 Given N, calculate F(N)
+
+Example 1:
+    Input: 2
+    Output: 1
+    Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1
 """
