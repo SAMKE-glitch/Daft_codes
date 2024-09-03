7f496cb64134fcb3d705949ea2fedfa396ce4643
samke-glitch <sammythemwa@gmail.com>
2024-09-03 21:29:14 +0300

    twoSum

---
+++ twoSum.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Given an array of integers, return indices of the two numbers such that 
+they add to a specific target.
+You may assume that each input would have exactly one solution, and you may 
+not use the same element twice
+"""
+
