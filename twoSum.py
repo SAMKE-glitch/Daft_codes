ad8771fea48a3579fe19d0972b71c8ed79caf75f
samke-glitch <sammythemwa@gmail.com>
2024-09-05 21:17:53 +0300

    redo

--- twoSum.py
+++ twoSum.py
@@ -16,7 +16,3 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # the for loop solution Bruteforce
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return([i, j])
