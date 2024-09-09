c107ea810cc187e565d78bfbc57dd7375219a448
samke-glitch <sammythemwa@gmail.com>
2024-09-09 21:41:11 +0300

    addTwo example

--- addTwo.py
+++ addTwo.py
@@ -5,4 +5,9 @@ The digits are stored in reverse order and each of their nodes contain a single 
 Add the two numbers and return it as a linked list.
 
 You may assume the two do not contain any leading zero, except for the number 0 itself
+
+Example:
+    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    Output: 7 -> 0 -> 8
+    Explanation: 342 + 465 = 807
 """
