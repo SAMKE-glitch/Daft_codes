c6c7e2ecd8882c8bd45eff53862b1e622e8c6b1d
samke-glitch <sammythemwa@gmail.com>
2024-09-09 21:39:03 +0300

    linked list add two numbers

---
+++ addTwo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+You are given two non-empty linked lists represents two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+
+You may assume the two do not contain any leading zero, except for the number 0 itself
+"""
