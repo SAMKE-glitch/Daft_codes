567c507e9e04309c8394b97db5b17d8683a0b1c8
samke-glitch <sammythemwa@gmail.com>
2024-09-11 17:13:54 +0300

    added

---
+++ ibm/stringS.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Consider a string, S that is a series of characters, each followed by its frequency
+as an integer. The string is not compressed correctly, so there may be multiple
+occurrences of the same character. A properly compressed string will consist of one instance
+of each character in alphabetical order followed by the total count of that character within a string
+"""
+#!/usr/bin/env python3
+"""
+Consider a string, S that is a series of characters, each followed by its frequency
+as an integer. The string is not compressed correctly, so there may be multiple
+occurrences of the same character. A properly compressed string will consist of one instance
+of each character in alphabetical order followed by the total count of that character within the string.
+"""
