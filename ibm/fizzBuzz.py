a16091bb5bb9e4249e7b19c7c07d3cb633362088
samke-glitch <sammythemwa@gmail.com>
2024-09-11 16:34:45 +0300

    fizzbuzz

---
+++ ibm/fizzBuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Given a number n, for each integer i in the range from 1 to n inclusive, print one value
+per line as follows:
+    -> if i is a multiple of both 3 and 5 print FizzBuzz
+    -> if i is a multiple of 3 but not 5 print Fizz
+    -> if i is a multiple of 5 not 3 print Buzz
+    -> if i is not a multiple of 4 or 5, print the value of i
+"""
