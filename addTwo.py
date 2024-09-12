5405cc1b4f2beb660695f749605e4e0306b08515
samke-glitch <sammythemwa@gmail.com>
2024-09-12 21:37:47 +0300

    uncomment

--- addTwo.py
+++ addTwo.py
@@ -12,9 +12,9 @@ Example:
     Explanation: 342 + 465 = 807
 """
 # Definition for singly-linked list
-# class ListNode:
-#   def __init__(self, val=0, next=None):
-#       self.val = val
-#       slf.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
