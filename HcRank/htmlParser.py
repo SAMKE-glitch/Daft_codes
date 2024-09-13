2e3084ece73a1675bec5bc3f9a71b76adbb39c0b
samke-glitch <sammythemwa@gmail.com>
2024-09-13 22:25:15 +0300

    question

--- HcRank/htmlParser.py
+++ HcRank/htmlParser.py
@@ -16,6 +16,50 @@ Format your results in the following way:
     -> Attribute4[0] > Attribute_value4[0]
     End   : Tag3
     End   : Tag2
+
+Here, the -> symbol indicates that the tag contains an attribute. It is immediately followed by the name of the attribute and the attribute value.
+The > symbol acts as a separator of the attribute and the attribute value.
+
+If an HTML tag has no attribute then simply print the name of the tag.
+If an attribute has no attribute value then simply print the name of the attribute value as None.
+
+Note: Do not detect any HTML tag, attribute or attribute value inside the HTML comment tags (<!-- Comments -->).Comments can be multiline as well.
+
+Input Format
+
+The first line contains integer N, the number of lines in a HTML code snippet.
+The next N lines contain HTML code.
+
+Constraints:
+    0 < N < 100
+
+Output Format
+
+Print the HTML tags, attributes and attribute values in order of their occurrence from top to bottom in the given snippet.
+
+Use proper formatting as explained in the problem statement.
+
+Sample Input:
+    2
+    <html><head><title>HTML Parser - I</title></head>
+    <body data-modal-target class='1'><h1>HackerRank</h1><br /></body></html>
+
+Sample Output:
+    Start : html
+    Start : head
+    Start : title
+    End   : title
+    End   : head
+    Start : body
+    -> data-modal-target > None
+    -> class > 1
+    Start : h1
+    End   : h1
+    Empty : br
+    End   : body
+    End   : html
+
+
 """
 from html.parser import HTMLParser
 
