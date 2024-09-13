e6673d650bd25733c5c284fe98b70a9521572926
samke-glitch <sammythemwa@gmail.com>
2024-09-13 22:22:49 +0300

    question

--- HcRank/htmlParser.py
+++ HcRank/htmlParser.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+"""
+You are given an HTML code snippet of N lines.
+Your task is to print start tags, end tags and empty tags separately.
+
+Format your results in the following way:
+    Start : Tag1
+    End   : Tag1
+    Start : Tag2
+    -> Attribute2[0] > Attribute_value2[0]
+    -> Attribute2[1] > Attribute_value2[1]
+    -> Attribute2[2] > Attribute_value2[2]
+    Start : Tag3
+    -> Attribute3[0] > None
+    Empty : Tag4
+    -> Attribute4[0] > Attribute_value4[0]
+    End   : Tag3
+    End   : Tag2
+"""
 from html.parser import HTMLParser
 
 # create a subclass and override the handler methods
