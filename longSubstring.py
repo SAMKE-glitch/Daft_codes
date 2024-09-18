b411ec237023921206adac537ec4feebf08eb3a3
samke-glitch <sammythemwa@gmail.com>
2024-09-18 21:50:11 +0300

    long substring

--- longSubstring.py
+++ longSubstring.py
@@ -12,4 +12,11 @@ Example 2:
     Input: "bbbbb"
     Output: 1
     Explanation: The answer is "b", with the length of 1
+
+Example 3:
+    Input:  "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+                NOTE-> That the answer must be a substring, "pwke" is 
+                a subsequence and not a substring.
 """
