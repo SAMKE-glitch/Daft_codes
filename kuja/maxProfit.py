88abb56e0cd71cd5b699178c1382983c507a037d
samke-glitch <sammythemwa@gmail.com>
2024-09-24 11:57:13 +0300

    maxProfit

---
+++ kuja/maxProfit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Your task is to write the function maxProfit that calculates the maximum
+profit achievable from these transactions (profit = selling price - buying price).
+If a profit isn't attainable, the function should return the lowest stock price
+of the day. The function takes two parameters as inputs - The prices array that contains the share price over the day (prices), and the number of entries in the prices array (n).
+It then calculates and returns the maximum profit that could have been achieved that day.
+
+Example 1:
+    Input: n=5
+           prices = [10,12,9,11,12]
+    Output: 3
+    Explanation: The lowest price is 9 and the highest price is 12
+                so 12 - 9 = 3
+""'
