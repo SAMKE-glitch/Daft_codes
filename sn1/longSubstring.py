9dd5fb280443cdce095313665a6b715983b94c5b
samke-glitch <sammythemwa@gmail.com>
2024-10-01 21:53:54 +0300

    if letter in sub

--- sn1/longSubstring.py
+++ sn1/longSubstring.py
@@ -25,7 +25,7 @@ class Solution:
         # we need to keep a dictionary that sores the letters we've seen
         sub = {}
         # we need to keep track of the index of our current substring is starting from
-        cur_sub_start = 0
+        cur_sub_startIndex = 0
         # current length of our substring we've seen so far
         cur_len = 0
         # longest substring so far
@@ -34,4 +34,6 @@ class Solution:
 
         # loop through every letter in string s and also need index position, so use enumerate
         for i, letter in enumerate(s):
+            if letter in sub and sub[letter] >= cur_sub_startIndex:
+
 
