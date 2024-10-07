0df98ab57e21ba3ef324ed00e0a976b523c15aad
samke-glitch <sammythemwa@gmail.com>
2024-10-07 21:46:16 +0300

    Example 1

--- sn1/PalindromeSubstring.py
+++ sn1/PalindromeSubstring.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 """
 Given a string s, return the longest palindromic substring in s
+
+Example:
+    Input: s = "babad"
+    Output: "bab"
+    Note: "aba" is also a valid answer
 """
