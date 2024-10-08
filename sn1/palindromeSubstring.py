b43d6e08e702f5da5b19034056578c921da1289e
samke-glitch <sammythemwa@gmail.com>
2024-10-08 21:46:03 +0300

    check helper method

--- sn1/palindromeSubstring.py
+++ sn1/palindromeSubstring.py
@@ -23,3 +23,8 @@ Example 5:
     1 <= s.length <= 1000
     s consist of only digits and English letters (lower-case and/or upper-case)
 """
+class Solution:
+    def longestPalindrome(self, s:str) -> str:
+
+        def check_palin(s):
+            return(s == s[::-1])
