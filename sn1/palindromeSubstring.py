07dad0c7eb2396b8d895313b5dfa6f869c0c581e
samke-glitch <sammythemwa@gmail.com>
2024-10-09 21:35:39 +0300

    bruteforce loop

--- sn1/palindromeSubstring.py
+++ sn1/palindromeSubstring.py
@@ -28,3 +28,10 @@ class Solution:
 
         def check_palin(s):
             return(s == s[::-1])
+
+        # check all substrings
+
+        for length in range(len(s), 0, -1):
+            for start_index in range(0, len(s)+1-length):
+                if check_palin(s[start_index:(start_inex+length)]):
+                    return s[start_index:(start_index+length)]
