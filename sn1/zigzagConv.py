37fc8e9edfe99724a8e3143715c10243fc9ba8ed
samke-glitch <sammythemwa@gmail.com>
2024-10-17 21:28:28 +0300

    zigzag

---
+++ sn1/zigzagConv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+The string "PAYPALISHIRING" is written in a zigzag patter on a given number of rows
+like this: (you may want to display this patter in a fixed font for better legibility)
+P   A
+A P L
+Y   I
+"""
+
