7fab55d9c3b34cf2c2a9ac05fd5ecc874a36c686
samke-glitch <sammythemwa@gmail.com>
2024-10-28 17:03:13 +0300

    efficient

--- sn1/palindromNu.py
+++ sn1/palindromNu.py
@@ -20,4 +20,31 @@ Example 3:
 """
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        """x = str(x)
+        return(x == x[::-1])
+        # we need to declare a variable to store the reversed digits
+        rev_digit = 0
+        # a variable to store the digit we're looking at
+        digit = 0
 
+        while(x // (10**digit) != 0):
+            rev_digit = (rev_digit * 10) + (x // (10**digit) % 10)
+            digit += 1
+        return(x == rev_digit)"""
+        # Early exit for negative numbers and numbers ending with 0 (except 0 itself)
+        if x < 0 or (x % 10 == 0 and x !=0):
+            return False
+
+        original = x # store the orginal number to compare later
+        rev_digit = 0
+
+        while x > 0:
+            rev_digit = rev_digit * 10 + x % 10 # Extract the last digit of x and add it
+            x //= 10 # Remove the last digit from x
+        return original == rev_digit
+
+
+sam = Solution()
+Enput = 121
+result = sam.isPalindrome(Enput)
+print(result)
