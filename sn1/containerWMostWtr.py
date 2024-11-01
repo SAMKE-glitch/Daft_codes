6fe797bc7ccba042fc9039a9b95aeb15332aaf99
samke-glitch <sammythemwa@gmail.com>
2024-11-01 17:21:02 +0300

    complete and instance

--- sn1/containerWMostWtr.py
+++ sn1/containerWMostWtr.py
@@ -20,3 +20,19 @@ class Solution:
         end = len(height) - 1
         largest = 0
 
+        while start != end:
+            next_area = min(height[start], height[end]) * (end - start)
+            if next_area > largest:
+                largest = next_area
+
+            if height[start] < height[end]:
+                start += 1
+            else:
+                end += 1
+        return largest
+
+sam = Solution()
+Input = [1,8,6,2,5,4,8,3,7]
+result = sam.maxArea(Input)
+print(result)
+
