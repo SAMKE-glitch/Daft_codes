11ead62fa48e4b206cb28ecf12c7e18618177bcc
samke-glitch <sammythemwa@gmail.com>
2024-11-01 17:37:19 +0300

    optimized

--- sn1/containerWMostWtr.py
+++ sn1/containerWMostWtr.py
@@ -19,15 +19,27 @@ class Solution:
         # variable to hold the last index
         end = len(height) - 1
         largest = 0
+        # parameters to store value previous height[start] and height[ends]
+        prev_start = 0
+        prev_end = 0
 
         while start != end:
+            if height[start] < prev_start:
+                start += 1
+                continue
+            if height[end] < prev_end:
+                end -= 1
+                continue
+
             next_area = min(height[start], height[end]) * (end - start)
             if next_area > largest:
                 largest = next_area
 
             if height[start] < height[end]:
+                prev_start = height[start]
                 start += 1
             else:
+                prev_end = height[end]
                 end -= 1
         return largest
 
