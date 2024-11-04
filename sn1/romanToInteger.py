9f8d24ac259cea3e50920a388d59865a3faafeae
samke-glitch <sammythemwa@gmail.com>
2024-11-04 20:39:47 +0300

    integr symbol;

--- sn1/romanToInteger.py
+++ sn1/romanToInteger.py
@@ -2,5 +2,14 @@
 """
 Roman numerals are represented by seven different symbols: I, V, X, L, C, D
 and M.
+Symbol  Value
+I       1
+V       5
+X       10
+L       50
+C       100
+D       500
+M       1000
 
+For example, 2
 """
