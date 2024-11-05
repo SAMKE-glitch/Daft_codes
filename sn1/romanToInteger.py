629dd0931b4f8903de807ab9b9000db1b1539d05
samke-glitch <sammythemwa@gmail.com>
2024-11-05 17:09:35 +0300

    example

--- sn1/romanToInteger.py
+++ sn1/romanToInteger.py
@@ -11,5 +11,7 @@ C       100
 D       500
 M       1000
 
-For example, 2 is written as II in Roman numeral, just two one's added together
+For example, 2 is written as II in Roman numeral, just two one's added together.
+12 is written as XII, which is simply X + II. The number 27 is written as XXVII,
+which is XX +V + II.
 """
