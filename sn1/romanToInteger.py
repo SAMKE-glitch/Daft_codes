11974dcd255d06f8a78338dd7e5b22faab293f19
samke-glitch <sammythemwa@gmail.com>
2024-11-06 16:36:44 +0300

    test

--- sn1/romanToInteger.py
+++ sn1/romanToInteger.py
@@ -54,4 +54,12 @@ Constraints:
 
 class Solution:
     def romanToInt(self, s: str) -. int:
-
+        roman_table = {"I": 1,
+                    "V": 5,
+                    "X": 10,
+                    "L": 50,
+                    "C": 100,
+                    "D": 500,
+                    "M": 1000
+                }
+        print(roman_table["I"])
