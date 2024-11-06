babf90d4932aa774c2871e389cada417743132cc
samke-glitch <sammythemwa@gmail.com>
2024-11-06 16:32:33 +0300

    class

--- sn1/romanToInteger.py
+++ sn1/romanToInteger.py
@@ -51,3 +51,7 @@ Constraints:
     -> s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
     -> It is guaranteed that s is a valid roman numeral in the range [1, 3999]
 """
+
+class Solution:
+    def romanToInt(self, s: str) -. int:
+
