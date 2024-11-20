592497f913f63e9bb0e08c8cd1f86e2b87c9cd0b
samke-glitch <sammythemwa@gmail.com>
2024-11-20 16:58:28 +0300

    range

--- sn1/3Sum.py
+++ sn1/3Sum.py
@@ -33,9 +33,9 @@ class Solution:
         triplets = []
         # n^3 Triple for loop solution
         # here we need to have 3 for loops
-        for i in range(len(nums)-2):
-            for j in range(len(nums)-1):
-                for k in range(len(nums)):
+        for i in range(0, len(nums)-2):
+            for j in range(i+1, len(nums)-1):
+                for k in range(j+1, len(nums)):
                     if nums[i] + nums[j] + nums[k] == 0:
                         triplets.append(tuple(set(nums[i] , nums[j] , nums[k])))
         return(triplets)
