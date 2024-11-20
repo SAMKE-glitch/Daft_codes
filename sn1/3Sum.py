7ee33327b0aa29d671224944c20f1dace2572835
samke-glitch <sammythemwa@gmail.com>
2024-11-20 17:02:56 +0300

    sorted

--- sn1/3Sum.py
+++ sn1/3Sum.py
@@ -36,8 +36,8 @@ class Solution:
         for i in range(0, len(nums)-2):
             for j in range(i+1, len(nums)-1):
                 for k in range(j+1, len(nums)):
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        triplets.append(tuple(set(nums[i] , nums[j] , nums[k])))
+                    if (nums[i] + nums[j] + nums[k]) == 0:
+                        triplets.append(tuple(sorted([nums[i] , nums[j] , nums[k]])))
         return(triplets)
 
 sam = Solution()
