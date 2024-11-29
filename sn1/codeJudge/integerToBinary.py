02d3ede17b97fb9caf99f98a1ec4df5a8817f113
samke-glitch <sammythemwa@gmail.com>
2024-11-29 16:35:23 +0300

    integrToBinary

---
+++ sn1/codeJudge/integerToBinary.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+function that converts integr to binary
+
+Example: 
+    Input: 23
+    Output : "10111"
+"""
+
+class Solution:
+    integerToBinary(self, number:int) -> str:
+        """
+        method to convert integer to binary
+        Input:
+            number(int)
+        Return:
+            binary(str)
+        """
+        if not isinstance(number, int):
+            raise ValueError("Insert integers only!!!")
+        
+        # return the converted number and sliced to remove the "ob" from "ob10111"
+        return bin(number)[2:]
