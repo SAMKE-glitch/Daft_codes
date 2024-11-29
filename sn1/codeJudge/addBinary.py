a0308e329c7ccd9ff8e09851a45d55d077d70d15
samke-glitch <sammythemwa@gmail.com>
2024-11-29 16:12:24 +0300

    add binary

--- sn1/codeJudge/addBinary.py
+++ sn1/codeJudge/addBinary.py
@@ -8,3 +8,12 @@ Example:
 """
 class Solution:
     def addBinary(self, a:str, b:str) -> str:
+        # convert the binary to integer for easy adding
+        # using base 2, because binaries are in base 2
+        bin1 = int(a, 2)
+        bin2 = int(b, 2)
+
+        total = bin1 + bin2
+
+        # convert back to binarries
+        return bin(total)[2:] # Here we have the slicing because the binaries starts with 0b10101
