7303179d2de5e5b83a9a4e5fe2125226bda57247
samke-glitch <sammythemwa@gmail.com>
2024-12-23 16:47:08 +0300

    Buzz

--- sn1/FizzBuzz.py
+++ sn1/FizzBuzz.py
@@ -2,3 +2,12 @@
 """
 FizzBuzz question
 """
+class Solution:
+    def fizzBuzz(self, n: int) -> str:
+        for i in range(1, n+1):
+            if (i % 3 == 0) and (i % 5 == 0):
+                print("FizzBuzz")
+            elif i % 3 == 0 and i % 5 != 0:
+                print("Fuzz")
+            elif i % 5 == 0 and i % 3 != 0:
+                print("Buzz")
