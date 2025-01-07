e1909ad7197ce9664725b50db7f518d78570f135
samke-glitch <sammythemwa@gmail.com>
2025-01-07 17:24:35 +0300

    return special case

--- sn1/threeSumClosset.py
+++ sn1/threeSumClosset.py
@@ -27,5 +27,11 @@ class Solution:
         for i in range(0, len(nums)-2):
             if nums[i] == nums[i-1] and i > 0:
                 continue
+
             lower = i+1
             upper = len(nums)-1
+
+            while lower < upper:
+                s = nums[i] + nums[lower] + nums[upper]
+                if s == target:
+                    return s
