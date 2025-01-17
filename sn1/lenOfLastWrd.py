4675acadf30720f5281dcd5afc0814c158345077
samke-glitch <sammythemwa@gmail.com>
2025-01-17 16:04:01 +0300

    lengthoflastword

---
+++ sn1/lenOfLastWrd.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Given a string s consists of some words separated by spaces,
+return the length of the last word in the string if the last word
+does not exist, return 0.
+
+A WORD is a maximal substring consisting of non-space characters only
+"""
