519347a53e4b4d9554aa972d63b63d3703e72d60
samke-glitch <sammythemwa@gmail.com>
2025-01-17 16:05:15 +0300

    example1

--- sn1/lenOfLastWrd.py
+++ sn1/lenOfLastWrd.py
@@ -4,5 +4,9 @@ Given a string s consists of some words separated by spaces,
 return the length of the last word in the string if the last word
 does not exist, return 0.
 
-A WORD is a maximal substring consisting of non-space characters only
+A WORD is a maximal substring consisting of non-space characters only.
+
+Example 1:
+    Input: s = "Hello World"
+    Output: 5
 """
