f2c0ffc1d4a0836db53f8550d9a96202b21001c3
samke-glitch <sammythemwa@gmail.com>
2025-01-28 20:58:26 +0300

    example 2

--- sn1/climbingStairs.py
+++ sn1/climbingStairs.py
@@ -11,4 +11,12 @@ Example 1:
 
     1. 1 step + 1 step
     2. 2 steps
+
+Example 2:
+    Input: 3
+    Output: 3
+    Explanation: There a re three ways to climb to the top.
+    1. 1 step + 1 step + 1 step
+    2. 1 step + 2 steps
+    3. 2 steps + 1 step
 """
