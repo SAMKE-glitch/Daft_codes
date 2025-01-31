7b5757ab7ca1ea478355134a4fdf6b4ede70734f
samke-glitch <sammythemwa@gmail.com>
2025-01-31 20:56:17 +0300

    binary

---
+++ sn1/maxDpthBinaryTree.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Given a binary tree, find its maximum depth.
+The maxima depth is the number of nodes along the longest path from the root
+node down to the farthest leaf node.
+
+Note: A leaf is a node with no childrea
+"""
