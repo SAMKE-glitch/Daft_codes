de52b54e2adbc420299b7be7102af9add9225c0e
samke-glitch <sammythemwa@gmail.com>
2025-01-31 20:57:34 +0300

    example

--- sn1/maxDpthBinaryTree.py
+++ sn1/maxDpthBinaryTree.py
@@ -4,5 +4,8 @@ Given a binary tree, find its maximum depth.
 The maxima depth is the number of nodes along the longest path from the root
 node down to the farthest leaf node.
 
-Note: A leaf is a node with no childrea
+Note: A leaf is a node with no children
+
+Example:
+    Given binary tree [2,9, null, null, 15, 7]
 """
