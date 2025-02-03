2c9b1e2a99980e25082cfdb87b963f538010f09c
samke-glitch <sammythemwa@gmail.com>
2025-02-03 20:33:42 +0300

    TreeNode class

--- sn1/maxDpthBinaryTree.py
+++ sn1/maxDpthBinaryTree.py
@@ -13,6 +13,13 @@ Example:
 """
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def maxDepth(self, root:TreeNode) -> int:
 
