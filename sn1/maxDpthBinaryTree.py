5d37ee43423b1529c9245be4225b2a0c34cacce5
samke-glitch <sammythemwa@gmail.com>
2025-02-03 20:51:44 +0300

    added

--- sn1/maxDpthBinaryTree.py
+++ sn1/maxDpthBinaryTree.py
@@ -22,4 +22,6 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root:TreeNode) -> int:
-
+        if not root:
+            return 0
+        return(1 + max(self.maxDepth(root.left), self.maxDepth(root.right)))
