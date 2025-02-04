bd465a92b0951a058e0fa91e2b6a21c691d0622f
samke-glitch <sammythemwa@gmail.com>
2025-02-04 21:03:12 +0300

    note &&example1

--- sn1/singleNumber.py
+++ sn1/singleNumber.py
@@ -2,4 +2,12 @@
 """
 Given a non-empty array of integers, every element appears twice except for one.
 Find that single one
+
+NOTE:
+    Your algorithm should have a linear runtime complexity. Could you implement it
+    without using extra memory?
+
+Example 1:
+    Input: [2,2,1]
+    Output: 1
 """
