d29210ec30d3f9e39ed0ae92938f0b5186e13e9f
samke-glitch <samythemwa@gmail.com>
2025-05-05 17:40:05 +0300

    object of fib

--- sn1/fibonacciNumber.py
+++ sn1/fibonacciNumber.py
@@ -30,4 +30,12 @@ Note:
 
 class Solution:
     def fib(self, N: int) -> int:
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+        return(self.fib(N- 1) + self.fib(N-2))
 
+samke = Solution()
+result = samke.fib(30)
+print(result)
