586ea1d0e6fcf3885fa26c7abc84c62fc4b685c3
samke-glitch <sammythemwa@gmail.com>
2025-05-27 17:24:51 +0300

    shebanga issue

--- sn1/sn2/singleNumber.py
+++ sn1/sn2/singleNumber.py
@@ -1,4 +1,4 @@
-#1/usr/bin/env python3
+#!/usr/bin/env python3
 """
 Given a non-empty array of integers, every element appears twice except for one.
 Find that single one.
@@ -24,10 +24,10 @@ class Solution:
 
         for num in nums:
             if num not in sums:
-                sums[num] += 1
+                sums[num] = 1
             else:
-                del(sums[num])
-        return list(sums.keys())
+                del(sums[num] )
+        return list(sums.keys())[0]
 
 samke = Solution()
 Input = [2, 2, 1]
