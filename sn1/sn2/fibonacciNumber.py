4bab1a2536c588fdf5339170ffa803c6b7b0b7b6
samke-glitch <sammythemwa@gmail.com>
2025-05-30 16:35:47 +0300

    fibonacci number

---
+++ sn1/sn2/fibonacciNumber.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called
+Fibonacci sequence, such that each number is the sum of the two preceding ones,
+starting from0 and 1.
+That is,
+    F(0) = 0, F(1) = 1
+    F(N) = F(N - 1) + F(N - 2), for N > 1.
+Given N, calculate F(N).
+"""
