696a5c3ba4890900bbcde491520cdf764396fc03
samke-glitch <sammythemwa@gmail.com>
2025-06-05 17:43:37 +0300

    seen

--- sn1/sn2/fibonacciNumber.py
+++ sn1/sn2/fibonacciNumber.py
@@ -30,6 +30,12 @@ NOTE:
 
 class Solution:
     def fib(self, N: int) -> int:
+        # Iterative solution
+        seen = {0:0, 1:1}
+
+        for i in range(2, N+1):
+            seen[i] = seen[i-1] + seen[i-2]
+        return(seen[N])
 
         # Recursion solution
         if N == 0:
