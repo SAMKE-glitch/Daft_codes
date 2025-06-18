e149e82648f062a3e66533ac389c649150598ef3
samke-glitch <sammythemwa@gmail.com>
2025-06-18 17:44:30 +0300

    added treeNode

--- sn1/sn2/maxDepthBinaryTree.py
+++ sn1/sn2/maxDepthBinaryTree.py
@@ -6,5 +6,19 @@ node down to the farthest leaf node
 Note: A leaf is anode with no children.
 
 Example:
-    Given binary tree [3, 9, 20, null, null, 15, 7]
+    Given binary tree [3, 9, 20, null, null, 15, 7],
+    
+    return its depth = 3
 """
+# Defination for a binary tree node
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
