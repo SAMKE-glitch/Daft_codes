7dcd076788362e2e01e6d4bc1bc0de9059a9fa78
samke-glitch <sammythemwa@gmail.com>
2025-06-23 17:33:51 +0300

    space optimized version

--- sn1/sn2/climbingStairs.py
+++ sn1/sn2/climbingStairs.py
@@ -24,3 +24,16 @@ Example 2:
 Constraints:
     1 <= n <= 45
 """
+from typing import List
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        a, b = 1, 2
+
+        for i in range(3, n+1):
+            a, b = b, a+b
+        return b
