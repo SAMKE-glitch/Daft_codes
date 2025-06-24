85c1df42c8b5a100f5737aa11bcf726d5d03ccc7
samke-glitch <sammythemwa@gmail.com>
2025-06-24 16:11:55 +0300

    added

--- sn1/sn2/sqrt.py
+++ sn1/sn2/sqrt.py
@@ -3,4 +3,12 @@
 Given a non-negative integer x, compute and return the square root of x.
 Since the return type is an integer, the decimal digits are truncated,
 and only the integer part of the result is return
+
+Example 1:
+    Input: x = 4
+    Output: 2
+
+Example 2:
+    Input: x = 8
+    Output: 2
 """
