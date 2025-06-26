e80e85cea884fc3252895bbc85af46f8a1f6a0ba
samke-glitch <sammythemwa@gmail.com>
2025-06-26 15:29:07 +0300

    question

--- sn1/sn2/plusOne.py
+++ sn1/sn2/plusOne.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 """
+Given a non-ampty array of decimal digits representing a non-negative integer,
+increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list,
+and each element in the array contains a single digit.
+You may assume the integer does not contain any leading zero, except the number 0 itself.
 """
