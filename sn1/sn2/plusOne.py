4cd579b5de75418ec05568c0491970751396b742
samke-glitch <sammythemwa@gmail.com>
2025-07-01 17:56:25 +0300

    added

--- sn1/sn2/plusOne.py
+++ sn1/sn2/plusOne.py
@@ -30,3 +30,5 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # Convoluted solution
+       return [int(x) for x in str(int("".join([str(i) for i in digits])) + 1)]
