f540340948222009a8f45599d73df28273e1c56d
samke-glitch <sammythemwa@gmail.com>
2025-07-09 16:12:29 +0300

    simple hash

---
+++ hashfuncs/simpleHash.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Demostration and understanding of Hash Functions and Hashing
+"""
+
+
+class Solution:
+    def simpleHash(inputString: String) -> int:
+        summation  sum(ord(ch) for ch in InputString)
+
+        # We limit our hash range from 0 to 9
+        return summation % 10
