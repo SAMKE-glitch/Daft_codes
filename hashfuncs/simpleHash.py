519f601d509cd89b0f653cfba175a1959ad7fad3
samke-glitch <sammythemwa@gmail.com>
2025-07-09 16:15:04 +0300

    syntax debug

--- hashfuncs/simpleHash.py
+++ hashfuncs/simpleHash.py
@@ -6,7 +6,7 @@ Demostration and understanding of Hash Functions and Hashing
 
 class Solution:
     def simpleHash(inputString: String) -> int:
-        summation  sum(ord(ch) for ch in InputString)
+        summation = sum(ord(ch) for ch in InputString)
 
         # We limit our hash range from 0 to 9
         return summation % 10
