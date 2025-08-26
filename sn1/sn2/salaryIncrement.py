7af62c1253ff870c35fa42141014062a62f90979
samke-glitch <sammythemwa@gmail.com>
2025-08-26 16:43:31 +0300

    salary increment

---
+++ sn1/sn2/salaryIncrement.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+Alright, Space Explorer, let's put that newfound knowledge of yours to use! Picture this: there's a company out there looking to reward their high-performing developers with a well-deserved 15% bump in salary. Your mission is to march into their employee database and make it happen. The database is just a list of dictionaries, with each dictionary serving as the ID card for an employee. A couple of keys to notice here are 'role' and 'salary': 'role' holds the job title, and 'salary' is the yearly paycheck. Your job? Simple: hunt down those developers and give their salary a 15% jet boost.
+
+Now, here's some technical jargon for you: the task involves iterating over a list of dictionaries. Each dictionary represents an employee, including their role and salary, among other attributes. You are to apply a 15% salary increment for employees with the role of 'developer'.
+
+Remember, it's important to preserve the integrity of the database and not to affect any data unrelated to the developer's salary. Best of luck, Voyager!
+"""
