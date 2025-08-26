583967fa3b1a1ba0fd0cfccdab87ebab4a683b74
samke-glitch <sammythemwa@gmail.com>
2025-08-26 17:23:47 +0300

    ditionary

--- sn1/sn2/letterCombinationsOnPhnmbrs.py
+++ sn1/sn2/letterCombinationsOnPhnmbrs.py
@@ -25,4 +25,6 @@ from typing import List
 
 
 class Solution:
-    def
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone_map = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+                '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
