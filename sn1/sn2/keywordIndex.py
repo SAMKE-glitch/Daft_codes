15ff94f232c99cfae64e569f2266ab3da5f4478b
samke-glitch <sammythemwa@gmail.com>
2025-08-27 16:53:43 +0300

    added

--- sn1/sn2/keywordIndex.py
+++ sn1/sn2/keywordIndex.py
@@ -2,3 +2,17 @@
 """
 In this situation, we are given a list of strings, with each string representing a document. Our task is to generate an index of all the distinct words in the documents for quick reference. We need to create a dictionary where each unique word is a key, and the corresponding value is a list of indices pointing to the documents where the word can be found.
 """
+from typing import List, Dict
+
+
+class Solution:
+    def keyword_index(self, docs: List[str]) -> Dict[str, List[int]]:
+        index: Dict[str, List[int]] = {}
+
+        for doc_idx, doc in enumerate(docs):
+            for word in doc.split():
+                if word in index:
+                    index[word].append(doc_idx)
+                else:
+                    index[word] = [doc_idx]
+        return index
