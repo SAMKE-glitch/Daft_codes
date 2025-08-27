2c1031b5e565537a0d2dfbc5fc284ba8a0fa016c
samke-glitch <sammythemwa@gmail.com>
2025-08-27 16:49:30 +0300

    added

---
+++ sn1/sn2/keywordIndex.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""
+In this situation, we are given a list of strings, with each string representing a document. Our task is to generate an index of all the distinct words in the documents for quick reference. We need to create a dictionary where each unique word is a key, and the corresponding value is a list of indices pointing to the documents where the word can be found.
+"""
