5b9f8e915c6e07d161756932f8798e5edec0c734
samke-glitch <sammythemwa@gmail.com>
2025-09-09 16:34:16 +0300

    index kyword

---
+++ sn1/sn2/indexKeyword.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Hey there, I've got a stellar task for you where you gotta be like a cosmic librarian! You know, we got gazillion documents in the universe, each represented as a string in a list. Your task is to whip up an index that's as nifty as a dictionary which logs in every unique word from these documents. Now, don't forget, each unique word is a key, mapping to another dictionary where the key is the document index and the value is how many times that word appeared in the document. It's sure gonna make searching a lot quicker, kinda like quantum speed, eh? Let's get rollin'!
+
+Ensure that your code handles all the edge cases. The input would be a list, where each string inside the list is a document. And every string is just a regular english text, easy peasy.
+
+The output should be a dictionary, where each unique word is a key, and the mapped value should be another dictionary, having document index as the key and its corresponding word count as the value.
+
+Alright, time to hit the hyper-speed!
+"""
