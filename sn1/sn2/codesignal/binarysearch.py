9c0871d077c646dadf1f0ad7ee139bad1f84bc41
samke-glitch <sammythemwa@gmail.com>
2025-09-17 16:38:11 +0300

    callign self method

--- sn1/sn2/codesignal/binarysearch.py
+++ sn1/sn2/codesignal/binarysearch.py
@@ -12,7 +12,7 @@ from typing import List
 class Solution:
     products_price = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
     def binary_search_iterative(self, data:List[int], target:int) -> int:
-        low = o
+        low = 0
         high = len(data)
 
         while high - low > 1:
@@ -28,9 +28,14 @@ class Solution:
         return low if data[low] == target else None
 
     def search_price(self, customer_query: int) -> int:
-        result = binary_search_iterative(products_price, customer_query)
+        result = self.binary_search_iterative(products_price, customer_query)
 
         if result is not None:
             print(f"Product of price ${customer_query} is found at position {result} in the price list.")
         else:
             print(f"No product is found with the price ${customer_query}.")
+
+
+samke = Solution()
+samke.search_price(30)
+samke.search_price(31)
