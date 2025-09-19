a43852c75dbd1c1d55e474fbac7835cd3af472da
samke-glitch <sammythemwa@gmail.com>
2025-09-19 16:52:56 +0300

    coomon prefix

---
+++ sn1/sn2/longestCommonPrefix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Write a function to find the longest common prefix string amongst an arrayof strings.
+If there is no common prefix, return an empty string ""
+
+Example 1:
+    Input: ["flower", "flow", "flight"]
+    Output: "fl"
+
+Example 2:
+    Input: ["dog", "racecar", "car"]
+    Output: ""
+    Explanation: there is no common prefix among the input strings
+
+Note: All given inputs are in lowercase letters a-z
+"""
