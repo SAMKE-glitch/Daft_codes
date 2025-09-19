99b9bdd5c62cb8bbe33d8e34fe55ebadf531a898
samke-glitch <sammythemwa@gmail.com>
2025-09-19 16:15:32 +0300

    added

---
+++ sn1/sn2/codesignal/studentPosition.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Excellent work, astronaut! As the final step in our exploration of Binary Search, you have a mission to undertake!
+
+Consider this exciting real-life scenario: you possess a sorted list of grades for a class, and a student is eager to ascertain his/her position within the list. Your assignment is to implement Binary Search on this list to locate the specific position of a given grade.
+
+Prepare yourself for an exciting journey!
+"""
+from typing import List
+
+
+class Solution:
+    grades = [35, 42, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+
