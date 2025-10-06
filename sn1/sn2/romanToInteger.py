658e09545dcbad9630a1f4d155f9ff2ffc073ba2
samke-glitch <sammythemwa@gmail.com>
2025-10-06 16:42:40 +0300

    examples

--- sn1/sn2/romanToInteger.py
+++ sn1/sn2/romanToInteger.py
@@ -23,4 +23,26 @@ where substraction is used:
     -> X can be placed before L (50) and C (100) to make 40 and 90.
     -> C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
+
+Example 1:
+    Input: s = "III"
+    Output: 3
+
+Example 2:
+    Input: s = "IV"
+    Output: 4
+
+Example 3:
+    Input: s = "IX"
+    Output: 9
+
+Example 4:
+    Input: s = "LVIII"
+    Output: 58
+    Explanation: L =50, V = 5, III = 3.
+
+Example 5:
+    Input: s = "MCMXCIV"
+    Output: 1994
+    Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 """
