413cb1c7ff9be94c1c8f73fe6eac03001367f00f
samke-glitch <sammythemwa@gmail.com>
2025-10-29 09:04:47 +0300

    pydantic model

--- sn1/sn2/projects/code/awesome-project/main.py
+++ sn1/sn2/projects/code/awesome-project/main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
 
 @app.get("/")
 async def read_root():
@@ -11,3 +16,7 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+async update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
