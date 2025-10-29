5184d8385482c59a094134dac3f21eaceedbed01
samke-glitch <sammythemwa@gmail.com>
2025-10-29 09:10:33 +0300

    added pydantic models

--- sn1/sn2/projects/code/awesome-project/main.py
+++ sn1/sn2/projects/code/awesome-project/main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,5 +19,5 @@ async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.put("/items/{item_id}")
-async update_item(item_id: int, item: Item):
+async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
