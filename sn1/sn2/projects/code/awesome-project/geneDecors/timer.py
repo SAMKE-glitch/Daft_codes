1ed9118fb8ffe4ac96fe2bd6f1aa3ae09184c2ba
samke-glitch <sammythemwa@gmail.com>
2025-10-30 09:17:36 +0300

    adding asterisk on kwargs

--- sn1/sn2/projects/code/awesome-project/geneDecors/timer.py
+++ sn1/sn2/projects/code/awesome-project/geneDecors/timer.py
@@ -2,9 +2,9 @@ import time
 
 
 def timer(func):
-    def wrapper(*args, *kwargs):
+    def wrapper(*args, **kwargs):
         start = time.time()
-        result = funct(*args, *kwargs)
+        result = funct(*args, **kwargs)
         end = time.time()
         print(f"{func.__name__} took {end - start:.4f} seconds")
         return func
