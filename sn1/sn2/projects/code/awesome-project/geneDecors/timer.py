be90b60444f0f8c11e204bde170ab5e88ecb6ee4
samke-glitch <sammythemwa@gmail.com>
2025-10-30 09:18:21 +0300

    func hot fox

--- sn1/sn2/projects/code/awesome-project/geneDecors/timer.py
+++ sn1/sn2/projects/code/awesome-project/geneDecors/timer.py
@@ -4,7 +4,7 @@ import time
 def timer(func):
     def wrapper(*args, **kwargs):
         start = time.time()
-        result = funct(*args, **kwargs)
+        result = func(*args, **kwargs)
         end = time.time()
         print(f"{func.__name__} took {end - start:.4f} seconds")
         return func
