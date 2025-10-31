abc77a87dc66adc6d5f41653289fea580d363229
samke-glitch <sammythemwa@gmail.com>
2025-10-31 08:28:45 +0300

    controller

---
+++ sn1/sn2/projects/code/awesome-project/app/controllers/item_controller.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, HTTPException
+from app.services.item_service import ItemService
+from app.dependencies.auth_dependency import get_user
+
+
+router = APIRouter()
+
+service = ItemService()
+
+@router.get("/items/{item_id}")
+def get_item(item_d: int, user: dict = Depends(get_user)):
+    try:
+        item = service.fetch_itemm(item_id)
+        return {"user": user["username"], "item": item}
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
