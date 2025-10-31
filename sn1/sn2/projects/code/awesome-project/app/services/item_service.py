986e95078d0102bfa109786544780f41db43ea19
samke-glitch <sammythemwa@gmail.com>
2025-10-31 09:20:18 +0300

    refacotred with sqlmodel

--- sn1/sn2/projects/code/awesome-project/app/services/item_service.py
+++ sn1/sn2/projects/code/awesome-project/app/services/item_service.py
@@ -1,12 +1,17 @@
 from app.repositories.item_repository import ItemRepository
+from app.models.item_model import Item
+from sqlmodel import Session
 
 
 class ItemService:
-    def __init__(self):
-        self.repo = ItemRepository()
+    def __init__(self, session: Session):
+        self.repo = ItemRepository(session)
 
     def fetch_item(self, item_id: int):
         item = self.repo.get_item(item_id)
         if not item:
             raise ValueError("Item not found")
         return item
+
+    def create_item(self, item: Item):
+        return self.repo.create_item(item)
