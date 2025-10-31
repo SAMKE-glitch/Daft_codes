76dcdebccb65597b3baae1333c3302a5472c419b
samke-glitch <sammythemwa@gmail.com>
2025-10-31 09:15:54 +0300

    sqlmodel on repository

--- sn1/sn2/projects/code/awesome-project/app/repositories/item_repository.py
+++ sn1/sn2/projects/code/awesome-project/app/repositories/item_repository.py
@@ -1,11 +1,17 @@
-# Simulated data layer
+from sqlmodel import Session, select
+from app.models.item_model import Item
+
 
 class ItemRepository:
-    def __init__(self):
-        self.items = {
-            1: {"name": "Laptop", "price": 1200.0},
-            2: {"name": "Phone", "price": 800.0}
-        }
+    def __init__(self, session: Session):
+        self.session = session
 
     def get_item(self, item_id: int):
-        return self.items.get(item_id, None)
+        return self.session.get(Item, item_id)
+
+    def create_item(self, item: Item):
+        self.session.add(item)
+        self.session.commit()
+        self.session.refresh(item)
+
+        return item
