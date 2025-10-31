bc71f19745030ee2e93c2475b419bd4a6f4d3866
samke-glitch <sammythemwa@gmail.com>
2025-10-31 08:56:37 +0300

    tested all routes

--- sn1/sn2/projects/code/awesome-project/app/controllers/item_controller.py
+++ sn1/sn2/projects/code/awesome-project/app/controllers/item_controller.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.services.item_service import ItemService
 from app.dependencies.auth_dependency import get_user
+from typing import Union
+from app.models.item_model import Item
 
 
 router = APIRouter()
-
 service = ItemService()
+
+@router.get("/")
+async def read_root(user: dict = Depends(get_user)):
+    return {"message": f"Welcome {user['username']}!"}
+
 
 @router.get("/items/{item_id}")
 def get_item(item_id: int, user: dict = Depends(get_user)):
@@ -14,3 +20,13 @@ def get_item(item_id: int, user: dict = Depends(get_user)):
         return {"user": user["username"], "item": item}
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
+
+# Extra route: query parameter optional
+@router.get("/items-query/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q":q}
+
+
+@router.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
