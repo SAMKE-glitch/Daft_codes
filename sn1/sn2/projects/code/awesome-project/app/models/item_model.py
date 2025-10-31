f7f1507ca57490781ebfa941dd2e986dcfb5a961
samke-glitch <sammythemwa@gmail.com>
2025-10-31 09:07:44 +0300

    added sqlmodel

--- sn1/sn2/projects/code/awesome-project/app/models/item_model.py
+++ sn1/sn2/projects/code/awesome-project/app/models/item_model.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 from typing import Optional
 
 
-class Item(BaseModel):
+class Item(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     price: float
     is_offer: Optional[bool] = None
